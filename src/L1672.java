3c93f9ee0cd69a60ada79ede7c9c2b0f00d79e2a
chetan16994 <chetan16994@gmail.com>
2021-01-04 14:54:37 -0700

    Initial commit

---
+++ src/L1672.java
@@ -0,0 +1,21 @@
+public class L1672 {
+    public int maximumWealth(int[][] arr) {
+        int maxSum=0;
+        if(arr.length==0) return 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum=0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum=sum+arr[i][j];
+            }
+            maxSum=Math.max(maxSum,sum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[][]={
+                {1,22,44},{12,33,14},{45,3,33}
+        };
+        L1672 obj=new L1672();
+        System.out.println(obj.maximumWealth(arr));
+    }
+}
