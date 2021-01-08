f9bc7a3b2a0c1c053bb6c399596218e6b8072802
chetan16994 <chetan16994@gmail.com>
2021-01-07 21:26:48 -0700

    .

---
+++ src/L1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class L1 {
+    public int[] twoSum(int[] nums, int target) {
+        int [] res=new int[2];
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]+nums[j]==target){
+                    res[0]=i;
+                    res[1]=j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        L1 obj=new L1();
+        int[] arr={2,7,11,15};
+        int target=9;
+        int[] res=obj.twoSum(arr,target);
+        for (int i:res)
+            System.out.println(i);
+    }
+}
